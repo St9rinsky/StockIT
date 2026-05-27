76a748a90d52ac7aedcda5278f3d9ae363c6cf02
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-05-27 20:03:07 +0200

    added my in memory db using lists for products

---
+++ src/main/java/za/co/sello/inventory/repository/ProductRepository.java
@@ -0,0 +1,26 @@
+package za.co.sello.inventory.repository;
+
+import za.co.sello.inventory.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class ProductRepository {
+    private final List<Product> products = new ArrayList<>();
+
+    public void save(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> findAll() {
+        return products;
+    }
+
+    public Optional<Product> findById(UUID id) {
+        return products.stream()
+                .filter(product -> product.getId().equals(id))
+                .findFirst();
+    }
+}
