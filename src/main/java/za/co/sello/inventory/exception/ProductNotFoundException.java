b894c56e6dd8e367d18246fc0f0335d1ea54b9df
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-05-27 21:56:55 +0200

    added a new exception for non existent products

---
+++ src/main/java/za/co/sello/inventory/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package za.co.sello.inventory.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
