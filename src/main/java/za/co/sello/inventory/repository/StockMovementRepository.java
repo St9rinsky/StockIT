348d20c2da38bdef830809890f9a6261409425e1
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-05-27 20:03:54 +0200

    added my in-memory stock movement db using lists

--- src/main/java/za/co/sello/inventory/repository/StockMovementRepository.java
+++ src/main/java/za/co/sello/inventory/repository/StockMovementRepository.java
@@ -1,4 +1,32 @@
 package za.co.sello.inventory.repository;
 
-public class ppp {
+import za.co.sello.inventory.model.StockMovement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class StockMovementRepository {
+    private final List<StockMovement> movements = new ArrayList<>();
+
+    public void save(StockMovement movement) {
+        movements.add(movement);
+    }
+
+    public List<StockMovement> findAll() {
+        return movements;
+    }
+
+    public List<StockMovement> findByProductId(UUID productId) {
+        return movements.stream()
+                .filter(movement -> movement.getProduct().getId().equals(productId))
+                .toList();
+    }
+
+    public List<StockMovement> findByProductIdAndLocationId(UUID productId, UUID locationId) {
+        return movements.stream()
+                .filter(movement -> movement.getProduct().getId().equals(productId))
+                .filter(movement -> movement.getLocation().getId().equals(locationId))
+                .toList();
+    }
 }
