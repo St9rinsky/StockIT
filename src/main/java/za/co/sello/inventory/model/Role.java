0f28532ba7929101c32691bba826926ae6164160
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-05-29 21:24:26 +0200

    added the num to represent different roles

--- src/main/java/za/co/sello/inventory/model/Role.java
+++ src/main/java/za/co/sello/inventory/model/Role.java
@@ -1,4 +1,10 @@
 package za.co.sello.inventory.model;
 
-public class Role {
+public enum Role {
+
+    //might need to change in the future so that pos rights can be adjusted
+
+    ADMIN, //can change roles
+    STOCK_MANAGER, //manages stock, adds and updates stuff
+    POS //point of sale can only remove products
 }
