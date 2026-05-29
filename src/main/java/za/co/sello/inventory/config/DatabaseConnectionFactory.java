ad18780c020501f0a2b10dcd6f2dd3570b13379e
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-05-29 22:06:06 +0200

    added the way to connect to the db

--- src/main/java/za/co/sello/inventory/config/DatabaseConnectionFactory.java
+++ src/main/java/za/co/sello/inventory/config/DatabaseConnectionFactory.java
@@ -1,4 +1,20 @@
 package za.co.sello.inventory.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnectionFactory {
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    DatabaseConfig.URL,
+                    DatabaseConfig.USER,
+                    DatabaseConfig.PASSWORD
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to connect to database", e);
+        }
+    }
 }
