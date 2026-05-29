c8fe3693f84ddc6917147dc4adf4c02655b4fe8c
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-05-29 21:27:34 +0200

    added the user class

--- src/main/java/za/co/sello/inventory/model/User.java
+++ src/main/java/za/co/sello/inventory/model/User.java
@@ -1,4 +1,24 @@
 package za.co.sello.inventory.model;
 
+import java.util.UUID;
+
 public class User {
+    private final UUID id;
+    private String name;
+    private Role role;
+
+    public User(String name, Role role) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.role = role;
+    }
+
+    public void updateRole(Role role){
+        this.role = role;
+    }
+    public void updateName(String name){this.name = name; }
+
+    public UUID getId() {return id;}
+    public String getName() {return name;}
+    public Role getRole() {return role;}
 }
