3a9d2495d0fb1b33c37950492482e587e4337c14
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-05-30 21:00:00 +0200

    added a user repository to store all my users

--- src/main/java/za/co/sello/inventory/repository/UserRepository.java
+++ src/main/java/za/co/sello/inventory/repository/UserRepository.java
@@ -1,4 +1,34 @@
 package za.co.sello.inventory.repository;
 
+
+import za.co.sello.inventory.model.Role;
+import za.co.sello.inventory.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public class UserRepository {
+    private final List<User> users = new ArrayList<>();
+
+    public void add(User user) {
+        users.add(user);
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public Optional<User> findById(UUID id) {
+        return users.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+    }
+
+    public List<User> findByRole(Role role){
+        return users.stream()
+                .filter(user -> user.getRole().equals(role))
+                .toList();
+    }
 }
