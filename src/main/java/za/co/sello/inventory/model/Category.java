183ccdd1945c2c2bcac5c0e3e1e286d8af62bbad
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-05-31 00:11:27 +0200

    added categorie class

--- src/main/java/za/co/sello/inventory/model/Category.java
+++ src/main/java/za/co/sello/inventory/model/Category.java
@@ -1,5 +1,6 @@
 package za.co.sello.inventory.model;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class Category {
@@ -27,8 +28,26 @@ public class Category {
         this.name = name;
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return Objects.equals(id, category.id)
+                && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
     @Override
     public String toString() {
         return name;
     }
+
 }
---
+++ src/test/java/za/co/sello/inventory/repository/JdbcCategoryRepositoryTest.java
@@ -0,0 +1,4 @@
+package za.co.sello.inventory.repository;
+
+public class JdbcCategoryRepositoryTest {
+}
