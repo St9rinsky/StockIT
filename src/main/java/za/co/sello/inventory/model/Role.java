399e7df9a911444057f983cd41b47e6083e67a81
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-06-14 00:24:38 +0200

    added user class and roles

--- src/main/java/za/co/sello/inventory/model/Role.java
+++ src/main/java/za/co/sello/inventory/model/Role.java
@@ -1,4 +1,8 @@
 package za.co.sello.inventory.model;
 
-public class Role {
+public enum Role {
+
+    ADMIN, //can change roles
+    STOCK_MANAGER, //manages stock, adds and updates stuff
+    POS //point of sale can only remove products
 }
--- src/main/java/za/co/sello/inventory/model/User.java
+++ src/main/java/za/co/sello/inventory/model/User.java
@@ -1,4 +1,43 @@
 package za.co.sello.inventory.model;
 
+import java.util.UUID;
+
 public class User {
+    private final UUID id;
+    private String name;
+    private final String companyId;
+    private Role role;
+
+    //current user
+    public User(UUID id, String name,String companyId, Role role) {
+        this.id = id;
+        this.name = name;
+        this.companyId = companyId;
+        this.role = role;
+    }
+
+    //new users
+    public User(String name,String companyId, Role role){this(UUID.randomUUID(), name,companyId,role);}
+
+    //getters
+    public UUID getId() {return id;}
+    public String getName() {return name;}
+    public String getCompanyId() {return companyId;}
+    public Role getRole() {return role;}
+
+    //setters
+    public void updateRole(Role role){
+        this.role = role;
+    }
+    public void updateName(String name){this.name = name; }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", companyId='" + companyId + '\'' +
+                ", role=" + role +
+                '}';
+    }
 }
