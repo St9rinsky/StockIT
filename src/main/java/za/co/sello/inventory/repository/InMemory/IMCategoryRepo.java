c40a6c4b03d75604fc8071fbb88b851abde5bf8a
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-06-14 03:22:31 +0200

    added the in memoryrepos for locations and categories

--- src/main/java/za/co/sello/inventory/repository/InMemory/IMCategoryRepo.java
+++ src/main/java/za/co/sello/inventory/repository/InMemory/IMCategoryRepo.java
@@ -1,4 +1,54 @@
 package za.co.sello.inventory.repository.InMemory;
 
-public class CategoryRepo {
+import za.co.sello.inventory.model.Category;
+import za.co.sello.inventory.repository.CategoryRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class IMCategoryRepo implements CategoryRepository {
+    private final List<Category> categories = new ArrayList<>();
+
+    @Override
+    public void add(Category category) {categories.add(category);}
+
+    @Override
+    public List<Category> getAll() {return categories;}
+
+    @Override
+    public Optional<Category> findById(UUID id) {
+        for (Category category : categories){
+            if(category.getId().equals(id)) return Optional.of(category);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Category> findByName(String name) {
+        for (Category category : categories){
+            if(category.getName().equals(name)) return Optional.of(category);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(Category category) {
+        for(int i = 0; i < categories.size(); i++) {
+            Category current = categories.get(i);
+
+            if (current.getId().equals(category.getId())) {
+                categories.set(i, category);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        Category category = this.findById(id).orElseThrow(() -> new RuntimeException("Category not found"));
+        categories.remove(category);
+
+    }
 }
--- src/main/java/za/co/sello/inventory/repository/InMemory/IMLocationRepo.java
+++ src/main/java/za/co/sello/inventory/repository/InMemory/IMLocationRepo.java
@@ -1,4 +1,54 @@
 package za.co.sello.inventory.repository.InMemory;
 
-public class LocationRepo {
+import za.co.sello.inventory.model.Location;
+import za.co.sello.inventory.repository.LocationRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class IMLocationRepo implements LocationRepository {
+    private final List<Location> locations = new ArrayList<>();
+
+    @Override
+    public void add(Location location) {locations.add(location);}
+
+    @Override
+    public List<Location> getAll() {return locations;}
+
+    @Override
+    public Optional<Location> findById(UUID id) {
+        for (Location location : locations){
+            if(location.getId().equals(id)) return Optional.of(location);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Location> findByName(String name) {
+        for (Location location : locations){
+            if(location.getName().equals(name)) return Optional.of(location);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(Location location) {
+        for(int i = 0; i < locations.size(); i++) {
+            Location current = locations.get(i);
+
+            if (current.getId().equals(location.getId())) {
+                locations.set(i, location);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        Location category = this.findById(id).orElseThrow(() -> new RuntimeException("Location not found"));
+        locations.remove(category);
+
+    }
 }
