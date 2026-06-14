f5a648ef42058499d77add781a87b2ceb3e0fc24
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-06-14 04:07:33 +0200

    added in memory product repo

--- src/main/java/za/co/sello/inventory/repository/InMemory/IMProductRepo.java
+++ src/main/java/za/co/sello/inventory/repository/InMemory/IMProductRepo.java
@@ -1,4 +1,63 @@
 package za.co.sello.inventory.repository.InMemory;
 
-public class ProductRepo {
+import za.co.sello.inventory.model.Location;
+import za.co.sello.inventory.model.Product;
+import za.co.sello.inventory.repository.ProductRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class IMProductRepo implements ProductRepository {
+    private final List<Product> products = new ArrayList<>();
+
+    @Override
+    public void add(Product product) {products.add(product);}
+
+    @Override
+    public List<Product> getAll() {return products;}
+
+    @Override
+    public Optional<Product> findById(UUID id) {
+        for(Product product: products){
+            if(product.getId().equals(id)) return Optional.of(product);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> findBySku(String sku) {
+        for(Product product: products){
+            if(product.getSKU().equals(sku)) return Optional.of(product);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Product> findByCategoryId(UUID categoryId) {
+        List<Product> selected = new ArrayList<>();
+        for(Product product: products){
+            if(product.getCategory().getId().equals(categoryId)) selected.add(product);
+        }
+        return selected;
+    }
+
+    @Override
+    public void update(Product product) {
+        for(int i = 0; i < products.size(); i++) {
+            Product current = products.get(i);
+
+            if (current.getId().equals(product.getId())) {
+                products.set(i, product);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+    Product product = this.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+    products.remove(product);
+    }
 }
